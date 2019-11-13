b2f618a9906aedf8383470c66cb30533eb0a34d9
Robinson Ramirez <robinson@camrpc.com>
2019-11-13 10:55:07 -0500

    Added quotes.

--- quotes.go
+++ quotes.go
@@ -1,6 +1,13 @@
 package quotes
 
 func Favs() []string {
-	var a []string
+	a := []string{
+		"Do or do not, there is no try.",
+		"May the force be with you.",
+		"Luke, I am your father.",
+		"I know.",
+		"It's a trap!",
+		"Eez tha peeple gonna die?",
+	}
 	return a
 }
